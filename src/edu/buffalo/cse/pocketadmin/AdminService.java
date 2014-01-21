47357ece411649d97d41daf671090e16ae572423
Jinghao Shi <jinghaos@buffalo.edu>
2014-01-21 16:51:31 -0500

    send scan results to AP

--- src/edu/buffalo/cse/pocketadmin/AdminService.java
+++ src/edu/buffalo/cse/pocketadmin/AdminService.java
@@ -1,8 +1,14 @@
 package edu.buffalo.cse.pocketadmin;
 
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.PendingIntent;
 import android.app.Service;
@@ -10,9 +16,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.ConnectivityManager;
 import android.net.DhcpInfo;
-import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
@@ -24,9 +28,12 @@ import android.util.Log;
 public class AdminService extends Service {
     private final String TAG = "PocketAdmin-" + this.getClass().getSimpleName();
 
-    private final String SSID = "\"PocketAdmin\"";
-    private final String PASSWORD = "\"AUDeORc1haIwvHuM6oeJOYhMAFYTffeC\"";
+    // wifi configuration
+    private final String SSID = "PocketAdmin";
+    private final String PASSWORD = "AUDeORc1haIwvHuM6oeJOYhMAFYTffeC";
     private final int PRIORITY = 99999999;
+
+    private final int PORT = 1688;
 
     private String checkIntentName = this.getClass().getName() + ".Check";
     private IntentFilter checkIntentFilter = new IntentFilter(checkIntentName);
@@ -34,37 +41,28 @@ public class AdminService extends Service {
     private BroadcastReceiver checkReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.v(TAG, "Checking");
-
-            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-
-            if (!wifi.isConnected()) {
-                Log.v(TAG, "Wifi not connected");
-            }
+            Log.v(TAG, "Checking...");
 
             WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-            DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
 
-            Log.v(TAG, 
-                    "dns1: " + int2Addr(dhcpInfo.dns1).toString() + "," + 
-                    "dns2: " + int2Addr(dhcpInfo.dns2).toString() + "," + 
-                    "Gateway: " + int2Addr(dhcpInfo.gateway).toString() +  "," + 
-                    "ipAddress: " + int2Addr(dhcpInfo.ipAddress).toString() + "," + 
-                    "netmask " + int2Addr(dhcpInfo.netmask).toString() + "," + 
-                    "Server: " + int2Addr(dhcpInfo.serverAddress).toString());
+            if (!wifiManager.isWifiEnabled()) {
+                Log.v(TAG, "Wifi not enabled");
+                return;
+            }
+
+
+            String ssid = "\"" + SSID + "\"";
 
             WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-            if (wifiInfo.getSSID().equals(SSID)) {
-                Log.v(TAG, "Already connected to PocketAdmin Wifi");
-            }
-            else {
+            // SSID in WifiInfo has quotes
+            if (!wifiInfo.getSSID().equals(ssid)) {
                 Log.v(TAG, "Currently connected to " + wifiInfo.getSSID());
 
                 boolean found = false;
 
                 List<ScanResult> scanResults = wifiManager.getScanResults();
                 for (ScanResult result : scanResults) {
+                    // SSID in ScanResults does NOT have quotes
                     if (result.SSID.equals(SSID)) {
                         found = true;
                         break;
@@ -79,10 +77,73 @@ public class AdminService extends Service {
                     wifiManager.enableNetwork(networkId, true);
                     wifiManager.reconnect();
                 }
+                else {
+                    Log.v(TAG, "No PocketAdmin Wifi found.");
+                }
+
+                /* In both cases, we need to wait for next check
+                 *  1) If we just initiated reconnection, the device need some
+                 *  time to connect
+                 *  2) Or we need to wait for PocketAdmin Wifi
+                 */
+
+                return;
+            }
+
+            /* At this point, we'r connected to PocketAdmin Wifi */
+
+            DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
+            InetAddress addr = int2Addr(dhcpInfo.gateway);
+            Log.v(TAG, "Sending msg to " + addr.toString());
+
+            try {
+                Socket socket = new Socket(addr, PORT);
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                printWriter.write(prepareMessage());
+                printWriter.flush();
+                printWriter.close();
+            }
+            catch (Exception e) {
+                Log.e(TAG, "Faild to send msg to router: " + e.getMessage());
             }
         }
+
     };
 
+
+    /* Package Wifi scan results and device's MAC address in to JSON format */
+    private String prepareMessage() {
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+        JSONObject msg = new JSONObject();
+
+        try {
+            msg.put("MAC", wifiInfo.getMacAddress());
+
+            JSONArray results = new JSONArray();
+            List<ScanResult> scanResults = wifiManager.getScanResults();
+            for (ScanResult result : scanResults) {
+                JSONObject json = new JSONObject();
+                json.put("SSID", result.SSID);
+                json.put("BSSID", result.BSSID);
+                json.put("frequency", result.frequency);
+                json.put("level", result.level);
+                json.put("timestamp", result.timestamp);
+                results.put(json);
+            }
+
+            msg.put("results", results);
+        }
+        catch (JSONException e) {
+            Log.e(TAG, "Faild to compose message: " + e.getMessage());
+        }
+
+        return msg.toString();
+    }
+
+
+    /* Convert integer to IPv4 address */
     private InetAddress int2Addr(int addr) {
         byte[] bytes = { 
             (byte)(0xff & addr),
@@ -99,25 +160,50 @@ public class AdminService extends Service {
         }
     }
 
+    /* Create wifi configuration for PocketAdmin Wifi if not exists already
+     * return its networkId
+     */
     private int addConfiguration() {
+        String ssid = "\"" + SSID + "\"";
+
         WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         List<WifiConfiguration> configurationList = wifiManager.getConfiguredNetworks();
-        for (WifiConfiguration config : configurationList) {
-            Log.v(TAG, config.SSID + ": " + config.priority);
-            if (config.SSID.equals(SSID)) {
-                return config.networkId;
+        for (WifiConfiguration con : configurationList) {
+            // SSID in WifiConfiguration has quotes
+            if (con.SSID.equals(ssid)) {
+                return con.networkId;
             }
         }
 
         WifiConfiguration config = new WifiConfiguration();
-        config.SSID = SSID;
-        config.preSharedKey = PASSWORD;
+        config.SSID = ssid;
+        // psk also has quotes
+        config.preSharedKey = "\"" + PASSWORD + "\"";
         config.priority = PRIORITY;
 
-        int networkId = wifiManager.addNetwork(config);
+        wifiManager.addNetwork(config);
+
+        /* From doc
+         *
+         *  "It is possible for this method to change the network IDs of existing
+         *  networks. You should assume the network IDs can be different after
+         *  calling this method."
+         *
+         * So we reload the configuration list to get the correct network id
+         * 
+         */
         wifiManager.saveConfiguration();
 
-        return networkId;
+        configurationList = wifiManager.getConfiguredNetworks();
+        for (WifiConfiguration con : configurationList) {
+            if (con.SSID.equals(ssid)) {
+                return con.networkId;
+            }
+        }
+
+        // Shouldn't be here
+        Log.e(TAG, "Can not find networkId");
+        return 0;
     }
 
 
