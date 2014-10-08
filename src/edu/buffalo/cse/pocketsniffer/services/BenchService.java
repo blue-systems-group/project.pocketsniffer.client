53974a5c24f6e37c90bb59928cea7aeb5bb289b8
Jinghao Shi <jinghaos@buffalo.edu>
2014-10-08 12:50:49 -0400

    add bench service

---
+++ src/edu/buffalo/cse/pocketsniffer/services/BenchService.java
@@ -0,0 +1,67 @@
+package edu.buffalo.cse.pocketsniffer.services;
+
+import java.util.ArrayList;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import edu.buffalo.cse.pocketsniffer.tasks.BatteryTask;
+import edu.buffalo.cse.pocketsniffer.tasks.SnifTask;
+import edu.buffalo.cse.pocketsniffer.utils.LocalUtils;
+
+public class BenchService extends Service {
+
+    private static final String TAG = LocalUtils.getTag(BenchService.class);
+
+    private Context mContext;
+    private BatteryTask mBatteryTask;
+    private SnifTask mSnifTask;
+    private boolean mStarted;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mStarted) {
+            Log.w(TAG, "Not starting BenchService.");
+            return START_STICKY;
+        }
+
+        super.onStartCommand(intent, flags, startId);
+
+        Log.d(TAG, "======== Starting BenchService =============");
+
+        mBatteryTask.start();
+
+        SnifTask.Params params = new SnifTask.Params();
+        params.channels = new ArrayList<Integer>();
+        for (int i = 1; i <= 11; i++) {
+            params.channels.add(i);
+        }
+        params.durationSec = 30*24*3600;
+        params.packetCount = -1;
+
+        Toast.makeText(mContext, "Sniffing", Toast.LENGTH_LONG).show();
+        mSnifTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
+
+        mStarted = true;
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = this;
+        mBatteryTask = new BatteryTask(mContext);
+        mSnifTask = new SnifTask(mContext, null);
+    }
+}
